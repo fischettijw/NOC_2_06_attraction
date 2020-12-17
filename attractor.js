39c1873cd42ee9c1de90b22edaaa2b5696356c8b
Joseph Fischetti <fischettijw@gmail.com>
2020-12-17 07:23:29 -0500

    initial

---
+++ attractor.js
@@ -0,0 +1,44 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// An object for a draggable attractive body in our world
+
+class Attractor {
+    constructor(x, y, m) {
+        this.position = createVector(x, y);
+        this.mass = m;
+        this.r = m * 4;
+        this.G = 1;
+        // this.dragOffset = createVector(0, 0);
+        // this.dragging = false;
+        // this.rollover = false;
+    }
+
+    attract(mover) {
+        // Calculate direction of force
+        let force = p5.Vector.sub(this.position, mover.position);
+        // Distance between objects
+        let distance = force.mag();
+        // Limiting the distance to eliminate "extreme" results for very close or very far objects
+        distance = constrain(distance, 5, 25);
+
+        // Calculate gravitional force magnitude
+        let strength = (this.G * this.mass * mover.mass) / (distance * distance);
+        // Get force vector --> magnitude * direction
+        force.setMag(strength);
+        mover.applyForce(force);
+        // return force;
+    }
+
+    // Method to display
+    show() {
+        ellipseMode(CENTER);
+        strokeWeight(4);
+        stroke(0);
+        fill('red');
+        circle(this.position.x, this.position.y, this.r);
+    }
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,22 @@
+let mover;
+// let mover2;
+let attractor;
+
+function setup() {
+    createCanvas(600, 700);
+    mover = new Mover(50, 50, 5);
+    // mover2 = new Mover(199, 100, 5);
+    attractor = new Attractor(width / 2, height / 2, 20);
+}
+
+function draw() {
+    background(220);
+    mover.update();
+    mover.show();
+    // mover2.update();
+    // mover2.show();
+
+    attractor.attract(mover);
+    // attractor.attract(mover2);
+    attractor.show();
+}
